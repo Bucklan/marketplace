d120dd8a069ff4af0ba6081ddfc4cd14385fb89f
assylzhan <As1kenjebaev@gmail.com>
2023-06-09 11:33:24 +0600

    add comment for API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,16 +22,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('/products',ProductController::class);
-Route::apiResource('/categories',CategoryController::class);
-Route::apiResource('/cart',CartController::class);
-
-Route::group(['middleware'=>'auth:sanctum'],function(){
-   Route::post('/logout',[AuthController::class,'logout']);
+Route::apiResource('/products', ProductController::class);
+Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/cart', CartController::class);
+Route::prefix('products')->as('products.')->group(function () {
+    Route::apiResource('/{products}/comments', CommentController::class);
 });
 
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 //Route::controller(AuthController::class)->group(function()
 //{
 //    Route::post('register', 'register');
